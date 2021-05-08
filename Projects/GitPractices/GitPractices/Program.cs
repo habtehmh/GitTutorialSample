fd55ee35ae428cceb380003064aac8e0af0e8741
Habtemichael Zeleke <habtehmh@gmail.com>
2021-05-07 19:42:07 -0500

    Development changes

---
+++ Projects/GitPractices/GitPractices/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing git commands - Dev branche added!");
+            Console.WriteLine("I am form Test branche");
+            Console.WriteLine("I am from Dev branche");
+        }
+    }
+}
