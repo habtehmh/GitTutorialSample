3fc6b5a06ee2a2cbbb6f42e8dcfc772838d0e8f4
Habtemichael Zeleke <habtehmh@gmail.com>
2021-05-07 20:18:09 -0500

    back to Test original

--- Projects/GitPractices/GitPractices/Program.cs
+++ Projects/GitPractices/GitPractices/Program.cs
@@ -10,9 +10,9 @@ namespace GitPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing git commands -Update Dev- Test branche added!");
+            Console.WriteLine("Testing git commands - Test branche added!");
             Console.WriteLine("I am form Test branche");
-            Console.WriteLine("I am from Dev branche");
+
         }
     }
 }
